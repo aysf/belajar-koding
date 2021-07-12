651800cf18fa881753a02fa13da0d8fe410d16ef
aysf <ansufw@gmail.com>
2021-07-12 15:52:41 +0700

    js update

--- update.sh
+++ update.sh
@@ -1,10 +1,7 @@
-
 ## add variable from argument after filename https://linuxhint.com/pass-argument-bash-script/
 comments="$1"
-
 ## add list of directories to variable https://stackoverflow.com/questions/18884992/how-do-i-assign-ls-to-an-array-in-linux-bash/18887210
-programs=($(ls -d */));
-
+programs=($(ls -d */))
 count=0
 while [ ${count} -lt ${#programs[@]} ]; do
     cd ${programs[count]}
@@ -15,7 +12,6 @@ while [ ${count} -lt ${#programs[@]} ]; do
     cd ..
     count=$((count+1))
 done
-
 git add .
 git commit -m "$comments"
 git push origin main
